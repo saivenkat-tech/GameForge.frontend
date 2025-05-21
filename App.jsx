ea43e7c4ffcbd4ed203cd40bdac3d1e29b546848
saivenkat-tech <psaivenkat101@gmail.com>
2025-05-21 14:48:23 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+    function App() {
+      return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+          <h1>Welcome to GameForge AI</h1>
+          <p>This is the starting App component.</p>
+        </div>
+      );
+    }
+
+    export default App;
